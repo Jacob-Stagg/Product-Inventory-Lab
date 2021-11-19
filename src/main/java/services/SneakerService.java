a0206f14dc2d31a08e912371c3d9233ec6b227df
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-11-19 16:18:22 -0500

    Added functionality to SneakerService.

--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,21 @@
 package services;
+import models.Sneaker;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SneakerService {
+    private static int nextId = 1;  // (1)
+    private List<Sneaker> inventory = new ArrayList<>();  // (2)
+
+    // (1)
+    public Sneaker create(String name, String brand, String sport, int size, int quantity, float price) {
+        // (2)
+        Sneaker createdSneaker = new Sneaker(nextId++, name, brand, sport, size, quantity, price);
+        // (3)
+        inventory.add(createdSneaker);
+        // (4)
+        return createdSneaker;
+    }
+
+
 }
